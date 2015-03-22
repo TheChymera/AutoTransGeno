42ad2a1a96de7feccb9077f9655443a002601a78
Horea Christian <h.chr@mail.ru>
2015-03-22 18:43:31 +0100

    user-definble sequence path

--- write.py
+++ write.py
@@ -3,17 +3,21 @@ __author__ = 'Horea Christian'
 
 from Bio import SeqIO
 
+sequence_write_path=""
 entrez_id=""
 # Please enter a list of ids for your sequences to use as filenames (you can pick whatever names you like)
 sequence_list=[]
 sequence_list_ids=[]
+
+if not sequence_write_path:
+	sequence_write_path=".cache/"
 
 if entrez_id:
 	from Bio import Entrez
 	Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
 	handle = Entrez.efetch(db="nucleotide", id=entrez_id, rettype="gb", retmode="text")
 	record = SeqIO.read(handle, "genbank")
-	SeqIO.write(record, ".cache/"+entrez_id+".fasta", "fasta")
+	SeqIO.write(record, sequence_write_path+entrez_id+".fasta", "fasta")
 
 if sequence_list:
 	from Bio.Seq import Seq
@@ -21,4 +25,4 @@ if sequence_list:
 	from Bio.SeqRecord import SeqRecord
 	for sequence_nr in enumerate(sequence_list):
 		cre_fw=SeqRecord(Seq(sequence_list[nr], generic_dna), id=sequence_list_ids[nr])
-		SeqIO.write(cre_fw, ".cache/"+sequence_list_ids[nr]+".fasta", "fasta")
+		SeqIO.write(cre_fw, sequence_write_path+sequence_list_ids[nr]+".fasta", "fasta")
