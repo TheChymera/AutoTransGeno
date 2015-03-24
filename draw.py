ffba4530c4b1c166d1d1bee7a3f2e6b800f8cac7
Horea Christian <h.chr@mail.ru>
2015-03-24 04:30:12 +0100

    added messy hard-code to process some data

--- draw.py
+++ draw.py
@@ -12,15 +12,15 @@ from Bio import SeqIO
 from Bio.Alphabet import generic_dna
 from reportlab.lib import colors
 
-def new_track(diagram, track_name):
+def new_track(diagram, track_name, smalltick=100, largetick=1000):
 	construct_track = diagram.new_track(
 						1,
 						name=track_name,
 						greytrack=True,
 						scale=True,
 						scale_format="SInt",
-						scale_smalltick_interval=100,
-						scale_largetick_interval=1000,
+						scale_smalltick_interval=smalltick,
+						scale_largetick_interval=largetick,
 						scale_fontsize=3,
 						scale_ticks=True,
 						scale_color=colors.black,
@@ -31,7 +31,35 @@ def new_track(diagram, track_name):
 	track_features = construct_track.new_set()
 	return construct_track, track_features
 
-def add_to_track(track_features, query, subject, annotation="", feature_color=""):
+def add_to_track(track_features, query, subject, annotation="", feature_color="", forceone=False, sigil="ARROW", label_angle=0):
+	from Bio.Blast.Applications import NcbiblastnCommandline
+
+	if forceone:
+		output = NcbiblastnCommandline(query=query, subject=subject, outfmt=5, task="blastn", num_alignments=1)()[0]
+	else:
+		output = NcbiblastnCommandline(query=query, subject=subject, outfmt=5, task="blastn")()[0]
+	blast_result_record = NCBIXML.read(StringIO(output))
+
+	for alignment in blast_result_record.alignments:
+		if forceone:
+			hsp_list=alignment.hsps[:1]
+		else:
+			hsp_list=alignment.hsps
+		for hsp in hsp_list:
+			if hsp.sbjct_start > hsp.sbjct_end:
+				strand = -1
+			else:
+				strand = +1
+			feature = SeqFeature(FeatureLocation(hsp.sbjct_start, hsp.sbjct_end), strand=strand)
+			track_features.add_feature(feature,
+				name=annotation+" ("+str(hsp.identities)+"/"+str(hsp.align_length)+")",
+				label=True, color=feature_color,
+				label_color=feature_color,
+				label_angle=label_angle,
+				sigil=sigil,
+				arrowshaft_height=1)
+
+def multitrack(diagram, track_name, query, subject, smalltick=100, largetick=1000, annotation="", feature_color=""):
 	from Bio.Blast.Applications import NcbiblastnCommandline
 
 	output = NcbiblastnCommandline(query=query, subject=subject, outfmt=5, task="blastn")()[0]
@@ -43,76 +71,113 @@ def add_to_track(track_features, query, subject, annotation="", feature_color=""
 				strand = -1
 			else:
 				strand = +1
+			construct_track = diagram.new_track(
+								1,
+								name=track_name,
+								greytrack=True,
+								scale=True,
+								scale_format="SInt",
+								scale_smalltick_interval=smalltick,
+								scale_largetick_interval=largetick,
+								scale_fontsize=3,
+								scale_ticks=True,
+								scale_color=colors.black,
+								scale_largeticks=0.4,
+								scale_smallticks=0.2,
+								axis_labels=True,
+								greytrack_labels=1)
+			track_features = construct_track.new_set()
 			feature = SeqFeature(FeatureLocation(hsp.sbjct_start, hsp.sbjct_end), strand=strand)
 			track_features.add_feature(feature,
-				name=annotation+str(hsp.identities)+"/"+str(hsp.align_length),
+				name=annotation+" ("+str(hsp.identities)+"/"+str(hsp.align_length)+")",
 				label=True, color=feature_color,
 				label_color=feature_color,
 				label_angle=0,
-				sigil="ARROW")
+				sigil="ARROW",
+				arrowshaft_height=1)
 
-datadir="/home/chymera/data/CreBLseq/Cre_aj627603/"
-cre_record = SeqIO.read(datadir+"aj627603.gbk", 'gb')
 
-gdd = GenomeDiagram.Diagram('Test Diagram', x=0.05, fragment_size=0.2)
-cre_construct_track = gdd.new_track(1, name="aj627603", greytrack=True, greytrack_labels=1)
+def cre(datadir, construct_name):
+	main_record = SeqIO.read(datadir+"aj627603.gbk", 'gb')
 
-primer_track, primer_features = new_track(gdd, "aj627603")
-add_to_track(primer_features, datadir+"preliminary_sequencing/primer_fw.fasta", datadir+"aj627603.fasta", annotation="fw_primer", feature_color=colors.green)
-add_to_track(primer_features, datadir+"cre_fw.fasta", datadir+"aj627603.fasta", annotation="cre_fw", feature_color=colors.green)
-add_to_track(primer_features, datadir+"cre_rv.fasta", datadir+"aj627603.fasta", annotation="cre_rv", feature_color=colors.green)
+	gdd = GenomeDiagram.Diagram(construct_name+' Construct Diagram', x=0.05, track_size=0.2)
 
-hit_track, hit_features = nre_track(gdd, "aj627603")
-gds_features = cre_construct_track.new_set()
-
-# ########## Run BLAST and add features for hit alignment
-output = NcbiblastnCommandline(query=datadir+"preliminary_sequencing/hit_full.fasta", subject=datadir+"aj627603.fasta", outfmt=5)()[0]
-blast_result_record = NCBIXML.read(StringIO(output))
-
-for alignment in blast_result_record.alignments:
-	print("a")
-	for hsp in alignment.hsps:
-		if hsp.sbjct_start > hsp.sbjct_end:
-			strand = -1
+	genbank_track, genbank_features = new_track(gdd, construct_name+" features")
+	for feature in main_record.features:
+		if feature.type != "gene":
+			#Exclude this feature
+			continue
+		if len(genbank_features) % 2 == 0:
+			color = colors.thistle
 		else:
-			strand = +1
-		feature = SeqFeature(FeatureLocation(hsp.sbjct_start, hsp.sbjct_end), strand=strand)
-		hit_features.add_feature(feature, name="hit_full_match "+str(hsp.identities)+"/"+str(hsp.align_length), label=True, color=colors.red, height=5)
-##########
+			color = colors.lightblue
+		genbank_features.add_feature(feature, sigil="ARROW", color=color, label_color=color, label=True, label_size = 14, label_angle=0, arrowshaft_height=1)
 
-for feature in cre_record.features:
-	if feature.type != "gene":
-		#Exclude this feature
-		continue
-	if len(gds_features) % 2 == 0:
-		color = colors.thistle
-	else:
-		color = colors.lightblue
-	gds_features.add_feature(feature, sigil="ARROW", color=color, label_color=color, label=True, label_size = 14, label_angle=0, arrowshaft_height=1)
+	restriction_track, restriction_features = new_track(gdd, construct_name+" restriction sites")
+	for site, name, color in restriction_list:
+		index = 0
+		while True:
+			index  = main_record.seq.find(site, start=index)
+			if index == -1 : break
+			feature = SeqFeature(FeatureLocation(index, index+len(site)))
+			restriction_features.add_feature(feature, color=color, name=name, label=True, label_size=3, label_position="end", label_color=color, label_angle=90)
+			index += len(site)
 
-#I want to include some strandless features, so for an example
-#will use EcoRI recognition sites etc.
-restriction_list=[
-	("GAATTC","EcoRI",colors.chartreuse),
-	("CCCGGG","SmaI",colors.orange),
-	("GTTAAC","HindI",colors.lemonchiffon),
-	("GTCGAC","HindI",colors.cornsilk),
-	("CAATC","HinfI",colors.olive),
-	("CATTC","HinfI",colors.olive),
-	("CAGTC","HinfI",colors.olive),
-	("CACTC","HinfI",colors.olive),
-	("AGCGCT","HaeII",colors.navy),
-	("GGCGCC","HaeII",colors.navy),
-	("GGATCC","BamHI",colors.purple)
-	]
-for site, name, color in restriction_list:
-	index = 0
-	while True:
-		index  = cre_record.seq.find(site, start=index)
-		if index == -1 : break
-		feature = SeqFeature(FeatureLocation(index, index+len(site)))
-		gds_features.add_feature(feature, color=color, name=name, label=True, label_size=3, label_position="end", label_color=color, label_angle=90)
-		index += len(site)
+	primer_track, primer_features = new_track(gdd, construct_name+" primers")
+	add_to_track(primer_features, datadir+"preliminary_sequencing/primer_fw.fasta", datadir+"aj627603.fasta", annotation="seq_fw_primer", feature_color=colors.green)
+	add_to_track(primer_features, datadir+"cre_fw.fasta", datadir+"aj627603.fasta", annotation="cre_fw_primer", feature_color=colors.red)
+	add_to_track(primer_features, datadir+"cre_rv.fasta", datadir+"aj627603.fasta", annotation="cre_rv_primer", feature_color=colors.red)
 
-gdd.draw(format='linear', pagesize="A4", fragments=1, start=0, end=len(cre_record))
-gdd.write("output/plasmid_linear_nice.pdf", "PDF")
+
+	hit_track, hit_features = new_track(gdd, construct_name+" hit alignments")
+	add_to_track(hit_features, datadir+"preliminary_sequencing/hit_fw.fasta", datadir+"aj627603.fasta", annotation="alignement with hit_fw", feature_color=colors.cornflower, sigil="BOX", label_angle=30)
+	add_to_track(hit_features, datadir+"preliminary_sequencing/hit_rv.fasta", datadir+"aj627603.fasta", annotation="alignement with hit_rv", feature_color=colors.salmon, sigil="BOX", label_angle=30)
+
+	gdd.draw(format='linear', pagesize="A4", fragments=1, start=0, end=len(main_record))
+	gdd.write("output/"+construct_name+"_diagram.pdf", "PDF")
+
+def my_hit(datadir, construct_name):
+	main_record = SeqIO.read(datadir+"preliminary_sequencing/hit_full.fasta", 'fasta')
+	gdd = GenomeDiagram.Diagram(construct_name+' Construct Diagram', x=0.05, track_size=0.2)
+
+	# multitrack(gdd, construct_name, datadir+"preliminary_sequencing/hit_fw.fasta", datadir+"preliminary_sequencing/hit_full.fasta", annotation="hit_fw", feature_color=colors.cornflower)
+	# multitrack(gdd, construct_name, datadir+"preliminary_sequencing/hit_rv.fasta", datadir+"preliminary_sequencing/hit_full.fasta", annotation="ht_rv", feature_color=colors.salmon)
+
+	hit_fw_track, hit_fw_features = new_track(gdd, construct_name+" forward hit", smalltick=10, largetick=100)
+	add_to_track(hit_fw_features, datadir+"preliminary_sequencing/hit_fw.fasta", datadir+"preliminary_sequencing/hit_full.fasta", annotation="hit_fw", feature_color=colors.cornflower, forceone=True, sigil="BOX")
+
+	hit_rv_track, hit_rv_features = new_track(gdd, construct_name+" reverse hit", smalltick=10, largetick=100)
+	add_to_track(hit_rv_features, datadir+"preliminary_sequencing/hit_rv.fasta", datadir+"preliminary_sequencing/hit_full.fasta", annotation="hit_rv", feature_color=colors.salmon, forceone=True, sigil="BOX")
+
+	primer_track, primer_features = new_track(gdd, construct_name+" primers", smalltick=10, largetick=100)
+	add_to_track(primer_features, datadir+"preliminary_sequencing/primer_fw.fasta", datadir+"preliminary_sequencing/hit_full.fasta", annotation="seq_fw_primer", feature_color=colors.green)
+
+	restriction_track, restriction_features = new_track(gdd, construct_name+" restriction sites", smalltick=10, largetick=100)
+	for site, name, color in restriction_list:
+		index = 0
+		while True:
+			index  = main_record.seq.find(site, start=index)
+			if index == -1 : break
+			feature = SeqFeature(FeatureLocation(index, index+len(site)))
+			restriction_features.add_feature(feature, color=color, name=name, label=True, label_size=5, label_position="end", label_color=color, label_angle=90)
+			index += len(site)
+
+	gdd.draw(format='linear', pagesize="A4", fragments=1, start=1, end=len(main_record))
+	gdd.write("output/"+construct_name+"_diagram.pdf", "PDF")
+
+if __name__ == '__main__':
+	restriction_list=[
+		("GAATTC","EcoRI",colors.chartreuse),
+		("CCCGGG","SmaI",colors.orange),
+		("GTTAAC","HindI",colors.magenta),
+		("GTCGAC","HindI",colors.purple),
+		("GAATC","HinfI",colors.fuchsia),
+		("GATTC","HinfI",colors.fuchsia),
+		("GAGTC","HinfI",colors.fuchsia),
+		("GACTC","HinfI",colors.fuchsia),
+		("AGCGCT","HaeII",colors.cyan),
+		("GGCGCC","HaeII",colors.cyan),
+		("GGATCC","BamHI",colors.turquoise)
+		]
+	cre(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="aj627603")
+	my_hit(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="my_full_hit")
