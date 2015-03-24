bf9e57c7837a06a7da7fea6dea55e08f09287e6b
Horea Christian <h.chr@mail.ru>
2015-03-24 14:23:22 +0100

    removed all hard-coded Cre text

--- write.py
+++ write.py
@@ -3,26 +3,26 @@ __author__ = 'Horea Christian'
 
 from Bio import SeqIO
 
-sequence_write_path=""
-entrez_id=""
-# Please enter a list of ids for your sequences to use as filenames (you can pick whatever names you like)
-sequence_list=[]
-sequence_list_ids=[]
+def write_seq(sequence_write_path=".cache/", entrez_id="", sequence="", sequence_id=""):
+	sequence_write_path="/home/chymera/data/CreBLseq/Cre_aj627603/preliminary_sequencing/"
 
-if not sequence_write_path:
-	sequence_write_path=".cache/"
+	if sequence and not sequence_id:
+		raise Exception("Please specify an ID for your sequence (whatever string you choose)")
 
-if entrez_id:
-	from Bio import Entrez
-	Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
-	handle = Entrez.efetch(db="nucleotide", id=entrez_id, rettype="gb", retmode="text")
-	record = SeqIO.read(handle, "genbank")
-	SeqIO.write(record, sequence_write_path+entrez_id+".fasta", "fasta")
+	if entrez_id:
+		from Bio import Entrez
+		Entrez.email = "A.N.Other@example.com"     # Always tell NCBI who you are
+		handle = Entrez.efetch(db="nucleotide", id=entrez_id, rettype="gb", retmode="text")
+		record = SeqIO.read(handle, "genbank")
+		destination = sequence_write_path+entrez_id+".fasta"
+		SeqIO.write(record, destination, "fasta")
 
-if sequence_list:
-	from Bio.Seq import Seq
-	from Bio.Alphabet import generic_dna
-	from Bio.SeqRecord import SeqRecord
-	for sequence_nr in enumerate(sequence_list):
-		cre_fw=SeqRecord(Seq(sequence_list[nr], generic_dna), id=sequence_list_ids[nr])
-		SeqIO.write(cre_fw, sequence_write_path+sequence_list_ids[nr]+".fasta", "fasta")
+	if sequence_list:
+		from Bio.Seq import Seq
+		from Bio.Alphabet import generic_dna
+		from Bio.SeqRecord import SeqRecord
+		record=SeqRecord(Seq(sequence, generic_dna), id=sequence_id)
+		destination = sequence_write_path+sequence_id+".fasta"
+		SeqIO.write(record, destination, "fasta")
+
+	return destination
