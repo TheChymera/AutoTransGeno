c54a0d17834036e550da738c4c8200ae1fbb0c31
Horea Christian <h.chr@mail.ru>
2015-03-29 06:09:44 +0200

    improved construct_on_templates to allow multiple construct hits

--- draw.py
+++ draw.py
@@ -13,23 +13,40 @@ from Bio.Alphabet import generic_dna
 from reportlab.lib import colors
 
 def new_track(diagram, track_name, smalltick=100, largetick=1000, track_no=1, endpoint=0):
-	construct_track = diagram.new_track(
-						track_no,
-						name=track_name,
-						greytrack=True,
-						scale=True,
-						scale_format="SInt",
-						scale_smalltick_interval=smalltick,
-						scale_largetick_interval=largetick,
-						scale_fontsize=3,
-						scale_ticks=True,
-						scale_color=colors.black,
-						scale_largeticks=0.4,
-						scale_smallticks=0.2,
-						axis_labels=True,
-						greytrack_labels=1,
-						start=0,
-						end=endpoint)
+	if endpoint != 0:
+		construct_track = diagram.new_track(
+							track_no,
+							name=track_name,
+							greytrack=True,
+							scale=True,
+							scale_format="SInt",
+							scale_smalltick_interval=smalltick,
+							scale_largetick_interval=largetick,
+							scale_fontsize=3,
+							scale_ticks=True,
+							scale_color=colors.black,
+							scale_largeticks=0.4,
+							scale_smallticks=0.2,
+							axis_labels=True,
+							greytrack_labels=1,
+							start=0,
+							end=endpoint)
+	else:
+		construct_track = diagram.new_track(
+							track_no,
+							name=track_name,
+							greytrack=True,
+							scale=True,
+							scale_format="SInt",
+							scale_smalltick_interval=smalltick,
+							scale_largetick_interval=largetick,
+							scale_fontsize=3,
+							scale_ticks=True,
+							scale_color=colors.black,
+							scale_largeticks=0.4,
+							scale_smallticks=0.2,
+							axis_labels=True,
+							greytrack_labels=1)
 	track_features = construct_track.new_set()
 	return construct_track, track_features
 
@@ -188,30 +205,48 @@ def my_hit(datadir, construct_name):
 	gdd.draw(format='linear', pagesize="A4", fragments=1, start=0, end=len(main_record))
 	gdd.write("output/"+construct_name+"_diagram.pdf", "PDF")
 
-def construct_on_templates(datadir, construct_name, templates):
-	from os.path import isfile
+def construct_on_templates(datadir, construct_names, templates, construct_is_file=False):
+	from os.path import isfile, splitext
 
-	main_record_file = datadir+construct_name+".gbk"
+	construct_files=[]
+	truncated_construct_names=[]
+	for construct in construct_names:
+		actual_name, ext = splitext(construct)
+		if ext in [".fasta", ".FASTA", ".gbk", ".GBK", ".genbank"]:
+			construct_files += [datadir+construct]
+			truncated_construct_names += [actual_name]
+		else:
+			construct_files += [datadir+construct+".gbk"]
+		if type(construct_files) is str:
+			construct_files = list(construct_files)
+	construct_names = truncated_construct_names
+
 	template_files = [datadir+"{0}.gbk".format(i) for i in templates]
+	files_list = construct_files+template_files
+	names_list = construct_names+templates
 
-	for entry in [construct_name]+templates:
-		if not isfile(datadir+entry+".gbk"):
+	for entry_name, entry_file in zip(names_list, files_list):
+		_, ext = splitext(entry_name)
+		if ext in [".fasta", ".FASTA", ".gbk", ".GBK", ".genbank"]:
+			pass
+		elif not isfile(entry_file):
 			from write import write_seq
-			write_seq(sequence_write_path=datadir, entrez_id=entry, formats=["genbank", "fasta"])
+			write_seq(sequence_write_path=datadir, entrez_id=entry_name, formats=["genbank", "fasta"])
 
-	gdd = GenomeDiagram.Diagram(construct_name+' Hits on Templates', x=0.05, track_size=0.2)
+	gdd = GenomeDiagram.Diagram(str(construct_names)+' Hits on Templates', x=0.05, track_size=0.2)
 
 	i = 0
 	max_len=0
 	for template_file, template in zip(template_files, templates):
 		template_record = SeqIO.read(template_file, "gb")
 		max_len = max(max_len, len(template_record))
-		template_track, template_features = new_track(gdd, construct_name+" primers", track_no=1, endpoint=len(template_record))
-		add_to_track(template_features, main_record_file, template_file, annotation=construct_name, feature_color=colors.deeppink, sigil="BOX")
+		template_track, template_features = new_track(gdd, ", ".join(map(str, construct_names))+" on "+template, track_no=1, endpoint=len(template_record))
+		for construct_name, construct_file in zip(construct_names, construct_files):
+			add_to_track(template_features, construct_file, template_file, annotation=construct_name, feature_color=colors.deeppink, sigil="BOX")
 		i += 1
 
 	gdd.draw(format='linear', pagesize="A4", fragments=1, start=0, end=max_len)
-	gdd.write("output/"+construct_name+"_template_hits.pdf", "PDF")
+	gdd.write("output/"+"_".join(map(str, construct_names))+"_template_hits.pdf", "PDF")
 
 if __name__ == '__main__':
 	restriction_list=[
@@ -225,9 +260,10 @@ if __name__ == '__main__':
 		# ("GACTC","HinfI",colors.fuchsia),
 		# ("AGCGCT","HaeII",colors.cyan),
 		# ("GGCGCC","HaeII",colors.cyan),
+		("CGGCCG","EagI",colors.chartreuse),
 		("GGCGCGCC","AscI",colors.deeppink),
 		("GGATCC","BamHI",colors.turquoise)
 		]
-	cre(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="U80929")
-	my_hit(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="my_full_hit")
-	construct_on_templates(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="HC686459", templates=["JC731547", "FJ493255", "JC731554"])
+	# cre(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="U80929")
+	# my_hit(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_name="my_full_hit")
+	construct_on_templates(datadir="/home/chymera/data/CreBLseq/Cre_aj627603/", construct_names=["cre_fw.fasta", "cre_rv.fasta"], templates=["AF298785", "AF298789", "aj627603"], construct_is_file=True)
