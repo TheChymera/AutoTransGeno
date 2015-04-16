2f902c78a282262fbc9c0d6c4d656b8ae3f8f182
Horea Christian <h.chr@mail.ru>
2015-04-16 21:22:11 +0200

    removed cre function

--- draw.py
+++ draw.py
@@ -102,16 +102,20 @@ def draw_digest1(track_features, restriction_dict, restriction_colors=""):
 			colors.chartreuse,
 			colors.deeppink,
 			colors.turquoise,
-			colors.fuchsia
+			colors.fuchsia,
+			colors.coral,
+			colors.yellow
 		]
 
 	restriction_colors = cycle(restriction_colors)
+	angle = cycle([120,90,60,30])
 	for enzyme, sites in restriction_dict.iteritems():
 		current_color = restriction_colors.next()
+		current_angle=angle.next()
 		overhang_length = len(overhangs(enzyme))
 		for site in sites:
 			feature = SeqFeature(FeatureLocation(site, site+overhang_length))
-			track_features.add_feature(feature, color=current_color, name=enzyme.__name__, label=True, label_size=3, label_position="end", label_color=current_color, label_angle=90)
+			track_features.add_feature(feature, color=current_color, name="      "+enzyme.__name__, label=True, label_size=3, label_position="end", label_color=current_color, label_angle=current_angle)
 
 
 def check_blast_format(sequence_path):
@@ -122,46 +126,6 @@ def check_blast_format(sequence_path):
 		from write import convert_seq
 		sequence_path = convert_seq(sequence_path, "genbank", "fasta")
 	return sequence_path
-
-def cre(datadir, construct_name):
-	from os.path import isfile
-
-	main_record_file = datadir+construct_name+".gbk"
-	# main_record_file
-
-	if not isfile(main_record_file):
-		from write import write_seq
-		write_seq(sequence_write_path=datadir, entrez_id=construct_name, formats=["genbank", "fasta"])
-
-	main_record = SeqIO.read(main_record_file, 'gb')
-
-	gdd = GenomeDiagram.Diagram(construct_name+' Construct Diagram', x=0.05, track_size=0.2)
-
-	genbank_track, genbank_features = new_track(gdd, construct_name+" features")
-	for feature in main_record.features:
-		if feature.type != "gene" and feature.type != "regulatory":
-			# Exclude nongene features
-			continue
-		if len(genbank_features) % 2 == 0:
-			color = colors.thistle
-		else:
-			color = colors.lightblue
-		genbank_features.add_feature(feature, sigil="ARROW", color=color, label_color=color, label=True, label_size = 14, label_angle=0, arrowshaft_height=1)
-
-	restriction_track, restriction_features = new_track(gdd, construct_name+" restriction sites")
-	draw_digest(restriction_features, main_record, restriction_list)
-
-	primer_track, primer_features = new_track(gdd, construct_name+" primers")
-	add_to_track(primer_features, datadir+"preliminary_sequencing/primer_fw.fasta", main_record_file, annotation="seq_fw_primer", feature_color=colors.green)
-	add_to_track(primer_features, datadir+"cre_fw.fasta", main_record_file, annotation="cre_fw_primer", feature_color=colors.red)
-	add_to_track(primer_features, datadir+"cre_rv.fasta", main_record_file, annotation="cre_rv_primer", feature_color=colors.red)
-
-	hit_track, hit_features = new_track(gdd, construct_name+" hit alignments")
-	add_to_track(hit_features, datadir+"cre_f1.fasta", main_record_file, annotation="cre_f1", feature_color=colors.cornflower, sigil="BOX", label_angle=30)
-	add_to_track(hit_features, datadir+"cre_r1.fasta", main_record_file, annotation="cre_r1", feature_color=colors.salmon, sigil="BOX", label_angle=30)
-
-	gdd.draw(format="linear", pagesize="A4", fragments=1, start=0, end=len(main_record))
-	gdd.write("output/"+construct_name+"_diagram.pdf", "PDF")
 
 def my_hit(datadir, construct_name):
 	main_record = SeqIO.read(datadir+"preliminary_sequencing/hit_full.fasta", 'fasta')
