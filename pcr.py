c75d327b8ab62303488174c1fa82ea268a10029b
Horea Christian <h.chr@mail.ru>
2015-05-06 19:05:53 +0200

    implemented multithreading

--- pcr.py
+++ pcr.py
@@ -15,7 +15,7 @@ assembly_subdir = "Primary_Assembly/assembled_chromosomes/FASTA/"
 
 sequence_path = genbank_local_dir + seqence_subdir + assembly_subdir
 
-def mock_pcr(template, primer_files="", primer_seqs="", primer_directory="", max_length=3000):
+def mock_pcr(template, primer_files="", primer_seqs="", primer_directory="", max_length=3000, evalue=0.1):
 	"""
 	Performs a rudimentary in-silico PCR, which returns the sequence and length of template fragments flanked by primers shother than max_length.
 	"""
@@ -52,7 +52,7 @@ def mock_pcr(template, primer_files="", primer_seqs="", primer_directory="", max
 	rv_primer_start_positions=[]
 	template_seq = SeqIO.read(template, "fasta")
 	for primer_file in primer_files:
-		output = NcbiblastnCommandline(query=template, subject=primer_file, outfmt=5, evalue=0.1, task="blastn")()[0]
+		output = NcbiblastnCommandline(query=template, subject=primer_file, outfmt=5, evalue=evalue, task="blastn")()[0]
 		blast_result_record = NCBIXML.read(StringIO(output))
 
 		for alignment in blast_result_record.alignments:
@@ -75,13 +75,35 @@ def mock_pcr(template, primer_files="", primer_seqs="", primer_directory="", max
 				return amplicon_length, template_seq[fw_primer_start:rv_primer_start].seq
 
 
-def genome_mock_pcr(genome_path, unspecific_insert=False, primer_files="", primer_seqs="", primer_directory="", max_length=3000):
-	amplicons = []
-	for fasta_file in os.listdir(genome_path):
-		_, extension = os.path.splitext(fasta_file)
-		if extension == ".fa":
-			amplicons += [mock_pcr(genome_path+fasta_file, primer_files=primer_files, primer_seqs=primer_seqs, primer_directory=primer_directory, max_length=max_length)]
-			print(fasta_file)
+def mock_pcr_star(a_b_c_d_e):
+    """Convert `mock_pcr([1,2,3,4,5,6])` to `mock_pcr(1,2,3,4,5,6)` call."""
+    return mock_pcr(*a_b_c_d_e)
+
+def genome_mock_pcr(genome_path, unspecific_insert=False, primer_files="", primer_seqs="", primer_directory="", max_length=3000, evalue=0.1, poolsize=8):
+	from multiprocessing.dummy import Pool as ThreadPool
+	pool = ThreadPool(poolsize)
+
+	fasta_files = [genome_path+fasta_file for fasta_file in os.listdir(genome_path) if os.path.splitext(fasta_file)[-1] == ".fa"]
+
+	# for i in (zip(fasta_files,
+	# 	itertools.repeat(primer_files),
+	# 	itertools.repeat(primer_seqs),
+	# 	itertools.repeat(primer_directory),
+	# 	itertools.repeat(max_length))):
+		# print i
+
+	amplicons = pool.map(mock_pcr_star,
+						zip(fasta_files,
+							itertools.repeat(primer_files),
+							itertools.repeat(primer_seqs),
+							itertools.repeat(primer_directory),
+							itertools.repeat(max_length),
+							itertools.repeat(evalue)))
+
+	#close the pool and wait for the work to finish
+	pool.close()
+	pool.join()
+
 	if unspecific_insert:
 		amplicons += [mock_pcr(unspecific_insert, primer_files=primer_files, primer_seqs=primer_seqs, primer_directory=primer_directory, max_length=max_length)]
 
@@ -90,4 +112,7 @@ def genome_mock_pcr(genome_path, unspecific_insert=False, primer_files="", prime
 if __name__ == '__main__':
 	template = "AAAAAAAAAAAAAAAGGGGGGCCCCCCCCAAAAAAAAAAAAGAGGTGCATACCTTTGCGCATCATCTCCCAGGCTTGGGCTTCCTTTAGGGTAACTGGCCGCCGCCATGTTGCAAACGGGAAGGAAATGAATGAACCGCCGTTATGAAATCTTGCTTAGGCCTTCCTTCTTCCTAGCTTGTGACTAACCTCATTCCTCTCGGCTGGGTGGAGTGTCCTTTATCCTGTAGGCCAGGTGATGCAAAGCACGGAAGCCGGCGAGAGTTCTACCTCACAATCTGTCTCACCTTATTAGCCTTAAAAGCCCTTGAGCCTTATTGTCCTCGGGCATAATGCGTATTCTAGATTATTCTCTGAAAATCAAAGCGGACTTACAGAGGTCCGCTTGACCTCCCAACCCCAGAGGTAGTTATGGCGTAGTGCAGAGCCGTGGGATGGGGAGCTGAGTCATGGTGGTTCTGAAAAGAAATTTTCCACCACAAAATGGCTCCTGTAGTAGCAGCCCCTTCCATCCCCTGCACTTCCCATCACAGCCTCGCACTGACCCAGGCCCTATAGGCCAGGATGTAAAGGTCATTAAGAGGATTGGGTGTCCCTGCGCCTCAGAATCCTGCCCTTCTCCCCGTTCCATCCTCCAGAAACCAGATCTCTCCACTCCGCCCTGATCTGAGGTTAAATTTAGCAGCACGGAAGCCGGCCGTGTGACCTTTCTGGATCTGGGGTCTGAGCGGGCTCTCCACCCTGCTCCCCCTACACACATCTGGGAAATCCCATTGACTTGCTCCGGCTCTCATTTTTGCCCGAGAAGAACAGGTGTTTCGCGAACGAGCCCTGGGATTAGGGTCGTGCCTTCGGCCGTTGGAAACCCCCCACATGTTTTCTCAGTCTTTCCCCTTAGTTCGAGGGACTTGGAGGACACAGGTGGGCCCGCCCTGTGCTGCTCACGCTGACCTTTAGCCTTGCCCTTTGAGCTTGCTGATGAATGAGTTCACAGGTCTGCCCTGTCCAGGGGGTGTAGCCTGAAGTCCAGCCATGCTGGAACAAACTTCCCAGGGCATGAGTGAT"
 	# print mock_pcr("/home/chymera/genbank/genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/Primary_Assembly/assembled_chromosomes/FASTA/chr6.fa", primer_seqs=["TAAACAGGCCCACTTGAAGG", "CACACCCATCACAAACATGG"], max_length=1000)
-	print genome_mock_pcr("/home/chymera/genbank/genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/Primary_Assembly/assembled_chromosomes/FASTA/", primer_seqs=["TAAACAGGCCCACTTGAAGG", "CACACCCATCACAAACATGG"], max_length=1000)
+	results = genome_mock_pcr("/home/chymera/genbank/genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/Primary_Assembly/assembled_chromosomes/FASTA/", primer_seqs=["TAAACAGGCCCACTTGAAGG", "CACACCCATCACAAACATGG"], max_length=1000)
+
+	for i in results:
+		print(i)
