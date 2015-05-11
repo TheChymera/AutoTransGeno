9ad5633c8e7c31a6e855dbdce3f24fa3a68f344f
Horea Christian <h.chr@mail.ru>
2015-05-11 16:56:40 +0200

    removed some cruft print and unused variable lines

--- digest_fragments.py
+++ digest_fragments.py
@@ -6,16 +6,11 @@ from write import write_seq, check_format
 from Bio import SeqIO
 from Bio import Restriction
 
-genbank_local_dir = "/home/chymera/genbank/"
-seqence_subdir = "genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/"
-assembly_subdir = "Primary_Assembly/assembled_chromosomes/FASTA/"
-
 def select_digest(sequence, rb, max_length=10000, min_length=100):
 
 	if isinstance(sequence, str):
 		sequence = Seq(sequence)
 	elif isinstance(sequence, SeqRecord.SeqRecord):
-		print("MUEEE")
 		sequence = sequence.seq
 
 	digest = getattr(Restriction, rb).catalyze(sequence)
