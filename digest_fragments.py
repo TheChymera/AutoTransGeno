f422beea57953cc9485b2a83b37ee544227e1521
Horea Christian <h.chr@mail.ru>
2015-05-12 23:24:03 +0200

    added gc percentage filter

--- digest_fragments.py
+++ digest_fragments.py
@@ -4,9 +4,10 @@ from Bio.Seq import Seq
 from StringIO import StringIO
 from write import write_seq, check_format
 from Bio import SeqIO
+from Bio.SeqUtils import GC
 from Bio import Restriction
 
-def select_digest(sequence, rb, max_length=10000, min_length=100, save_to=False):
+def select_digest(sequence, rb, max_length=10000, min_length=100, max_gc=100, min_gc=0, save_to=False):
 
 	if isinstance(sequence, str):
 		sequence = Seq(sequence)
@@ -17,13 +18,19 @@ def select_digest(sequence, rb, max_length=10000, min_length=100, save_to=False)
 
 	selected_fragments = []
 	for ix, fragment in enumerate(digest):
-		if min_length <= len(fragment) <= max_length:
+		if min_length <= len(fragment) <= max_length and min_gc <= GC(fragment) <= max_gc:
 			selected_fragments += [fragment]
 			if save_to:
-				write_seq(sequence_write_path=save_to, entrez_id="", sequence=fragment, sequence_id=str(len(fragment))+"-"+str(ix), formats=["fasta"])
+				import time
+				from os import makedirs, path
+				append_date = "/"+time.strftime("%Y%m%d_%H%M")+"/"
+				save_path = save_to + append_date
+				if not path.exists(save_path):
+					makedirs(save_path)
+				write_seq(sequence_write_path=save_path, entrez_id="", sequence=fragment, sequence_id=str(len(fragment))+"-"+str(ix), formats=["fasta"])
 
 	return selected_fragments
 
 if __name__ == '__main__':
 	template = SeqIO.read("/home/chymera/genbank/genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/Primary_Assembly/assembled_chromosomes/FASTA/chr1.fa", "fasta")
-	results = select_digest(template, "XceI", min_length=800, max_length=2500, save_to="digests/")
+	results = select_digest(template, "XceI", min_length=1500, max_length=1500, max_gc=55, min_gc=30, save_to="digests/")
