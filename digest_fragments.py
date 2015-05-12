10e01172b5f636f0f7d95812dee663ac7b9caf24
Horea Christian <h.chr@mail.ru>
2015-05-12 02:56:22 +0200

    implemented file saving for fragments

--- digest_fragments.py
+++ digest_fragments.py
@@ -6,7 +6,7 @@ from write import write_seq, check_format
 from Bio import SeqIO
 from Bio import Restriction
 
-def select_digest(sequence, rb, max_length=10000, min_length=100):
+def select_digest(sequence, rb, max_length=10000, min_length=100, save_to=False):
 
 	if isinstance(sequence, str):
 		sequence = Seq(sequence)
@@ -16,13 +16,14 @@ def select_digest(sequence, rb, max_length=10000, min_length=100):
 	digest = getattr(Restriction, rb).catalyze(sequence)
 
 	selected_fragments = []
-	for fragment in digest:
+	for ix, fragment in enumerate(digest):
 		if min_length <= len(fragment) <= max_length:
 			selected_fragments += [fragment]
+			if save_to:
+				write_seq(sequence_write_path=save_to, entrez_id="", sequence=fragment, sequence_id=str(len(fragment))+"-"+str(ix), formats=["fasta"])
 
 	return selected_fragments
 
 if __name__ == '__main__':
 	template = SeqIO.read("/home/chymera/genbank/genomes/Eukaryotes/vertebrates_mammals/Mus_musculus/GRCm38.p3/Primary_Assembly/assembled_chromosomes/FASTA/chr1.fa", "fasta")
-	results = select_digest(template, "XceI", min_length=200, max_length=5000)
-	print[str(i) for i in results]
+	results = select_digest(template, "XceI", min_length=800, max_length=2500, save_to="digests/")
