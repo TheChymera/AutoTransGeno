ba5ecf9824e2ff66973545827fbb25893645b313
Horea Christian <h.chr@mail.ru>
2015-06-19 21:26:40 +0200

    using meldeld sequence

--- src/AutoTransGeno/cre.py
+++ src/AutoTransGeno/cre.py
@@ -1,7 +1,9 @@
 __author__ = "Horea Christian"
 
+from os.path import splitext
+
 def cre(datadir, feature_name, extract_from, hit_dir=""):
-	from utils import extract_feature, check_fetch_record
+	from utils import extract_feature, check_fetch_record, write_seq, simple_sequence_merge
 	from restriction import enzyme_selector
 	from Bio import SeqIO
 	from Bio.Graphics import GenomeDiagram
@@ -39,17 +41,25 @@ def cre(datadir, feature_name, extract_from, hit_dir=""):
 			["cre_fw7", "cre_rv7", colors.salmon]
 		]
 
+	# creating sequencing meld
+	ID_list = ["783476", "old_783477", "783477"]
+	base_dir = "/home/chymera/"
+	seq_paths = [base_dir + ID + ".fasta" for ID in ID_list]
+	sequences = [SeqIO.read(one_path, "fasta") for one_path in seq_paths]
+	outp = simple_sequence_merge(sequences=sequences)
+	seq_meld = write_seq(sequence=outp)
+
 	# plotting primers
 	primer_track, primer_features = new_track(gdd, construct_name+" primers", smalltick=10)
 	for primer_entry in primer_list:
 			add_to_track(primer_features, datadir+"primers/"+primer_entry[0]+".fasta", main_record_file, annotation=primer_entry[0], feature_color=primer_entry[2], label_angle=60)
 			add_to_track(primer_features, datadir+"primers/"+primer_entry[1]+".fasta", main_record_file, annotation=primer_entry[1], feature_color=primer_entry[2], label_angle=60)
 
-	for i in [0,1]:
-		hit_track_back, hit_features_back = new_track(gdd, str(783476+i), smalltick=10, end=len(SeqIO.read(hit_dir+str(783476+i)+".fasta", 'fasta')))
-		add_to_track(hit_features_back, main_record_file, hit_dir+str(783476+i)+".fasta", annotation=" "+construct_name, feature_color=colors.darkslateblue, label_angle=30, forceone=True)
+	for i in [hit_dir+"783476.fasta",hit_dir+"783477.fasta", seq_meld]:
+		hit_track_back, hit_features_back = new_track(gdd, splitext(i)[0][-6:], smalltick=10, end=len(SeqIO.read(i, 'fasta')))
+		add_to_track(hit_features_back, main_record_file, i, annotation=" "+construct_name, feature_color=colors.darkslateblue, label_angle=30, forceone=True)
 		hit_track, hit_features = new_track(gdd, construct_name+" alignment hits", smalltick=10)
-		add_to_track(hit_features, hit_dir+str(783476+i)+".fasta", main_record_file, annotation=" "+str(783476+i), feature_color=colors.darksalmon, label_angle=30)
+		add_to_track(hit_features, i, main_record_file, annotation=" "+splitext(i)[0][-6:], feature_color=colors.darksalmon, label_angle=30)
 
 	gdd.draw(format="linear", pagesize="A4", fragments=1, start=0, end=len(main_record))
 	gdd.write("/home/chymera/src/AutoTransGeno/output/"+construct_name+"_from_"+extract_from+".pdf", "PDF")
